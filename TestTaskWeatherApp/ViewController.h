59f1afbbb0af9b469f3bfbb8f75f057b522cec0a
AndyOrphan <andyorphan@icloud.com>
2018-05-17 02:15:17 +0300

    Initial Commit

---
+++ TestTaskWeatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestTaskWeatherApp
+//
+//  Created by Orphan on 5/17/18.
+//  Copyright © 2018 Orphan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestTaskWeatherApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TestTaskWeatherApp
+//
+//  Created by Orphan on 5/17/18.
+//  Copyright © 2018 Orphan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
